86a4ce7ade30bf4c455f846ae51312da2d2ff617
Chris Gramme <cgramme36@gmail.com>
2015-01-24 23:45:36 -0500

    thesite

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+$(document).ready(function(){
+	
+	$('h1').fadeIn(700);
+
+	setTimeout(function(){
+    	$('.fa-lightbulb-o').css({'text-shadow' :'0px 0px 20px #ffffff','color':'#ffff00'});
+    	$('h2').slideDown();
+	}, 700);
+
+});
