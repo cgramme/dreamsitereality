8bf950336d2676c2e66344a1a94a87c7c126544c
Chris Gramme <cgramme36@gmail.com>
2015-01-30 07:05:48 -0500

    radiation steve

--- business.js
+++ business.js
@@ -1,5 +1,25 @@
 
 
 $(document).ready(function(){
+
 	$('.nav-info').fadeIn(2000);
+	
+
+
+	$('.details,.about').mouseenter(function(){
+         
+         $('.side-options li:first-child').removeClass('hover-on');
+         
+         if($(this).hasClass('details')){
+         	$('.nav-info').hide();
+         	$('.nav-details').slideDown();
+         }else{
+         	$('.nav-info').hide();
+         	$('.nav-about').slideDown();
+         }
+	}).mouseleave(function(){
+		$('.side-options li:first-child').addClass('hover-on');
+		$('.nav-details,.nav-about').hide();
+		$('.nav-info').slideDown();
+	});
 });
